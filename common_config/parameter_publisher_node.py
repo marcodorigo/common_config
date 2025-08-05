ab148c28155b512f7bc20843068b0a8ea519d92e
marcodorigo <marco3.dorigo@mail.polimi.it>
2025-08-05 17:57:45 +0200

    Changed obstacle and target location

--- common_config/parameter_publisher_node.py
+++ common_config/parameter_publisher_node.py
@@ -8,9 +8,9 @@ class ParameterPublisherNode(Node):
         super().__init__('parameter_publisher_node')
 
         # Define constants
-        self.OBSTACLE_CENTER = [0.35, 0.365, 0.45]
+        self.OBSTACLE_CENTER = [0.5, 0.24, 0.5]
         self.OBSTACLE_RADIUS = 0.1
-        self.TARGET_POSITION = [0.2, 0.6, 0.4]
+        self.TARGET_POSITION = [0.4, 0.4, 0.5]
         self.WORKSPACE_RADIUS = 0.85
 
         # Publishers
